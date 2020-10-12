19293728a94a02728d1eeb5e8981a32d8b40717c
George Florea Bănuș <georgefb899@gmail.com>
2020-10-12 18:08:59 +0300

    Don't call resetOpenGLState() at the end of MpvRenderer::render() the application often crashes when it's called

--- src/mpvobject.cpp
+++ src/mpvobject.cpp
@@ -53,7 +53,7 @@ void MpvRenderer::render()
     // other API details.
     mpv_render_context_render(obj->mpv_gl, params);
 
-    obj->window()->resetOpenGLState();
+//    obj->window()->resetOpenGLState();
 }
 
 QOpenGLFramebufferObject * MpvRenderer::createFramebufferObject(const QSize &size)
